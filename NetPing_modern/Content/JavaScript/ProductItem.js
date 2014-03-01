31b7acad599ad1eaf6e40f9308d8a0f4076cf5ab
Sergey <sergey.sahakyan@picasart.com>
2014-03-01 15:13:45 +0400

    first modified

---
+++ NetPing_modern/Content/JavaScript/ProductItem.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function () {
+    $('.content .tabs-desc .tab').on('click', function () {
+        $(this).addClass('active').parent().find('.active').not(this).removeClass('active');
+        $('.tab-c[data-tab-id=' + $(this).data('tab-id') + ']').addClass('shown').parent().find('.shown').not('.tab-c[data-tab-id=' + $(this).data('tab-id') + ']').removeClass('shown');
+    });
+
+});
