97e5086fcac6b3bf9ce44576a29bedb45ce17510
Sergey <sergey.sahakyan@picasart.com>
2014-03-02 00:55:46 +0400

    ostalos tolko design navigatorov slidera i button zoom na slidere

--- NetPing_modern/Content/JavaScript/ProductItem.js
+++ NetPing_modern/Content/JavaScript/ProductItem.js
@@ -3,5 +3,4 @@
         $(this).addClass('active').parent().find('.active').not(this).removeClass('active');
         $('.tab-c[data-tab-id=' + $(this).data('tab-id') + ']').addClass('shown').parent().find('.shown').not('.tab-c[data-tab-id=' + $(this).data('tab-id') + ']').removeClass('shown');
     });
-
 });
---
+++ NetPing_modern/Content/JavaScript/slider1.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+    $("#slider3").responsiveSlides({
+        manualControls: '#slider3-pager',
+        auto: true,
+        speed: 500,
+        timeout: 3000,
+        nav: true,
+        random: false,
+        pause: true,
+        navContainer: "#slider3-pager-nav",
+        maxwidth: 540,
+        before: function () {
+            $('#slider3-pager').animate({
+                scrollLeft: $('#slider3-pager').scrollLeft() + $(".rslides_here").offset().left - $('#slider3-pager').offset().left - 75
+        }, 600);
+        }
+    });
+});
