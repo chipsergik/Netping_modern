2b33ff99737646d8ad582a72030f910940e01f53
Sergey <sergey.sahakyan@picasart.com>
2014-03-02 12:08:29 +0400

    minor fixing

--- NetPing_modern/Content/JavaScript/slider1.js
+++ NetPing_modern/Content/JavaScript/slider1.js
@@ -1,8 +1,8 @@
-﻿$(function () {
+﻿$(function() {
     $("#slider3").responsiveSlides({
         manualControls: '#slider3-pager',
         auto: true,
-        speed: 500,
+        speed: 400,
         timeout: 3000,
         nav: true,
         random: false,
@@ -12,7 +12,12 @@
         before: function () {
             $('#slider3-pager').animate({
                 scrollLeft: $('#slider3-pager').scrollLeft() + $(".rslides_here").offset().left - $('#slider3-pager').offset().left - 75
-        }, 600);
+            }, 450);
+        },
+        after: function() {
+            $('#slider3-pager').animate({
+                scrollLeft: $('#slider3-pager').scrollLeft() + $(".rslides_here").offset().left - $('#slider3-pager').offset().left - 75
+            }, 350);
         }
     });
 });
