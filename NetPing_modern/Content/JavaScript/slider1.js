a925af6a5ce5013b8b207ed52dadcc1948b7c864
Sergey <sergey.sahakyan@picasart.com>
2014-03-06 02:25:13 +0400

    zoom popup compleated

--- NetPing_modern/Content/JavaScript/slider1.js
+++ NetPing_modern/Content/JavaScript/slider1.js
@@ -20,4 +20,32 @@
             }, 350);
         }
     });
+
+    $('#searchIcon').click(function () {
+        var img = $($('.rslides1_on').find('img')[0]);
+        console.log(img);
+        if (img) {
+            img = img.clone();
+            img.removeClass();
+            img.addClass('popupImage');
+            $('#popup1').empty();
+            $('#popup1').append(["<span class='close'></span>", img]);
+            $('#popup1').show();
+            $('.overlay').show();
+            $('.close').click(function () {
+                $('.overlay, .popup').hide();
+            });
+        }
+    });
+
+    $('.popup .close, .overlay').click(function() {
+        $('.overlay, .popup').hide();
+    });
+    
+    $(document).keyup(function (e) {
+        if (e.keyCode == 27) {
+            $('.overlay, .popup').hide();
+        }
+    });
+    
 });
