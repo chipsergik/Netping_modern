e5edff00d4022c378634555bb8b55c332a072ec0
Andrew Kulevsky <kuljok@gmail.com>
2014-05-17 18:41:53 +0300

    Fixed lists on the short descriptions
    fixed JS issues on the main and catalog page

--- NetPing_modern/Content/JavaScript/main.js
+++ NetPing_modern/Content/JavaScript/main.js
@@ -33,5 +33,10 @@ $(document).ready(function($){
 	});
 	};
 
-    $('.tab-c.longdescription h3').sshutter();
+
+    var headers3 = $('.tab-c.longdescription h3');
+    if (headers3.length > 0)
+    {
+        headers3.sshutter();
+    }
 });
