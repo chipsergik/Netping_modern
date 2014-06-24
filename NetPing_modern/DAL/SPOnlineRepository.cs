6b52274e87301a72e5b7746d404acb6218c8e92f
Andrew Kulevsky <kuljok@gmail.com>
2014-06-24 08:27:16 +0300

    Uncommed UpdateAll

--- NetPing_modern/DAL/SPOnlineRepository.cs
+++ NetPing_modern/DAL/SPOnlineRepository.cs
@@ -319,7 +319,7 @@ namespace NetPing.DAL
         {
             try
             {
-                /*var termsLabels = TermsLabels_Read(); Debug.WriteLine("TermsLabels_Read OK");
+                var termsLabels = TermsLabels_Read(); Debug.WriteLine("TermsLabels_Read OK");
                 var termsDeviceParameters = TermsDeviceParameters_Read(); Debug.WriteLine("TermsDeviceParameters_Read OK");
                 var termsFileTypes = TermsFileTypes_Read(); Debug.WriteLine("TermsFileTypes_Read OK");
                 var termsDestinations = TermsDestinations_Read(); Debug.WriteLine("TermsDestinations_Read OK");
@@ -345,7 +345,7 @@ namespace NetPing.DAL
                 PushToCache("PubFiles", pubFiles);
                 PushToCache("SFiles", sFiles);
                 PushToCache("Posts", posts);
-                PushToCache("Devices", devices);*/
+                PushToCache("Devices", devices);
                 GeneratePriceList();
             }
             catch (Exception ex)
