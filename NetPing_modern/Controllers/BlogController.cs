910d0bdc99f9db888df55cf4d71f3f1966583f6a
chipsergik <chipsergik@gmail.com>
2014-09-11 09:50:07 +0400

    Blog layouting. Mainpage

---
+++ NetPing_modern/Controllers/BlogController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NetPing_modern.Controllers
+{
+    public class BlogController : Controller
+    {
+        //
+        // GET: /Blog/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Main()
+        {
+            return View();
+        }
+
+        public ActionResult Record()
+        {
+            return View();
+        }
+	}
+}
