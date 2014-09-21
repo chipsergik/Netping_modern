02ea1d84b15b4ceff1b50073f538c19f2fde3a65
chipsergik <chipsergik@gmail.com>
2014-09-21 13:44:08 +0400

    Blog layouting

---
+++ NetPing_modern/Content/JavaScript/blog.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    $('#datepicker').datetimepicker(
+        {
+            format: 'd.m.Y',
+            inline: true,
+            lang: 'ru',
+            timepicker: false,
+            todayButton: false
+        });
+
+});
