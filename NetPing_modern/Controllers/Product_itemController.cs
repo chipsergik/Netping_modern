9a9ec58bbff07cc4bc1ecca5e9655792b3836d80
chipsergik <chipsergik@gmail.com>
2014-11-13 12:17:37 +0300

    Development page layouting

--- NetPing_modern/Controllers/Product_itemController.cs
+++ NetPing_modern/Controllers/Product_itemController.cs
@@ -27,8 +27,8 @@ namespace NetPing.Controllers
             var connected_devices = device.Connected_devices.Select(d =>
                                                                            repository.Devices.Where(dv => dv.Name == d).FirstOrDefault()
                                                                       ).ToList();
-            ViewBag.Connected_devices_accessuars = connected_devices.Where(d => d!=null && !d.Name.Path.Contains("Sensors")).ToList();
-            ViewBag.Connected_devices_sensors = connected_devices.Where(d => d!=null &&  d.Name.Path.Contains("Sensors")).ToList();
+            ViewBag.Connected_devices_accessuars = connected_devices.Where(d => d != null && !d.Name.Path.Contains("Sensors")).ToList();
+            ViewBag.Connected_devices_sensors = connected_devices.Where(d => d != null && d.Name.Path.Contains("Sensors")).ToList();
 
             ViewBag.Parameter_groups = repository.TermsDeviceParameters.Where(par => par.Level == 0).ToList();
             ViewBag.Files_groups = repository.TermsFileTypes.Where(type => type.Level == 0).ToList();
@@ -36,7 +36,7 @@ namespace NetPing.Controllers
             //Device group
             var dev_path = device.Name.Path.Split(';');
             var grp = dev_path[dev_path.Length - 2];
-            var group_dev=repository.Devices.FirstOrDefault(dev => dev.Name.OwnNameFromPath == grp);
+            var group_dev = repository.Devices.FirstOrDefault(dev => dev.Name.OwnNameFromPath == grp);
             ViewBag.grp_name = group_dev.Name.Name;
             ViewBag.grp_url = group_dev.GroupUrl;
 
@@ -45,5 +45,9 @@ namespace NetPing.Controllers
             return View(device);
         }
 
+        public ActionResult Development()
+        {
+            return View();
+        }
     }
 }
