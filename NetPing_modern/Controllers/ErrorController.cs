4db11d955755230a2ecb06478fa933a032e65817
Andrew <kuljok@gmail.com>
2014-12-18 09:31:50 +0300

    [#2838685] Custom Page for Error 404

---
+++ NetPing_modern/Controllers/ErrorController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace NetPing_modern.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ViewResult NotFound()
+        {
+            Response.StatusCode = 404;
+            return View("NotFound");
+        }
+
+        public ViewResult Error()
+        {
+            return View("Error");
+        }
+    }
+}
